b08c61f0021656714022262b8f50618a17ad4b1f
xheruha <johnverorozco02@gmail.com>
2025-12-01 23:22:59 +0800

    sadb

--- FormLeaderboard.Designer.vb
+++ FormLeaderboard.Designer.vb
@@ -25,6 +25,11 @@ Partial Class FormLeaderboard
         Me.dvLB = New System.Windows.Forms.DataGridView()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cmbSub = New System.Windows.Forms.ComboBox()
+        Me.btnOV = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.cmbTerm = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -32,8 +37,6 @@ Partial Class FormLeaderboard
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnFilter = New System.Windows.Forms.Button()
         Me.cmbSect = New System.Windows.Forms.ComboBox()
-        Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.btnClear = New System.Windows.Forms.Button()
         CType(Me.dvLB, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
@@ -59,6 +62,9 @@ Partial Class FormLeaderboard
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.Wheat
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.cmbSub)
+        Me.Panel1.Controls.Add(Me.btnOV)
         Me.Panel1.Controls.Add(Me.btnClear)
         Me.Panel1.Controls.Add(Me.Panel3)
         Me.Panel1.Controls.Add(Me.Label3)
@@ -73,12 +79,64 @@ Partial Class FormLeaderboard
         Me.Panel1.Size = New System.Drawing.Size(259, 485)
         Me.Panel1.TabIndex = 25
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label4.Font = New System.Drawing.Font("Noto Sans JP", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(32, 113)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 25)
+        Me.Label4.TabIndex = 41
+        Me.Label4.Text = "Subject:"
+        '
+        'cmbSub
+        '
+        Me.cmbSub.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbSub.FormattingEnabled = True
+        Me.cmbSub.Location = New System.Drawing.Point(120, 113)
+        Me.cmbSub.Name = "cmbSub"
+        Me.cmbSub.Size = New System.Drawing.Size(122, 24)
+        Me.cmbSub.TabIndex = 40
+        '
+        'btnOV
+        '
+        Me.btnOV.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnOV.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOV.Font = New System.Drawing.Font("Noto Sans JP", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOV.Location = New System.Drawing.Point(153, 387)
+        Me.btnOV.Name = "btnOV"
+        Me.btnOV.Size = New System.Drawing.Size(89, 34)
+        Me.btnOV.TabIndex = 39
+        Me.btnOV.Text = "Overall"
+        Me.btnOV.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Red
+        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClear.Font = New System.Drawing.Font("Noto Sans JP", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(153, 159)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(89, 34)
+        Me.btnClear.TabIndex = 38
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.DarkKhaki
+        Me.Panel3.Location = New System.Drawing.Point(3, 209)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(261, 10)
+        Me.Panel3.TabIndex = 25
+        '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.Label3.Font = New System.Drawing.Font("Noto Sans JP", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(54, 112)
+        Me.Label3.Location = New System.Drawing.Point(54, 65)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(60, 25)
         Me.Label3.TabIndex = 37
@@ -88,7 +146,7 @@ Partial Class FormLeaderboard
         '
         Me.cmbTerm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmbTerm.FormattingEnabled = True
-        Me.cmbTerm.Location = New System.Drawing.Point(120, 113)
+        Me.cmbTerm.Location = New System.Drawing.Point(120, 66)
         Me.cmbTerm.Name = "cmbTerm"
         Me.cmbTerm.Size = New System.Drawing.Size(122, 24)
         Me.cmbTerm.TabIndex = 36
@@ -98,7 +156,7 @@ Partial Class FormLeaderboard
         Me.Label2.AutoSize = True
         Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.Label2.Font = New System.Drawing.Font("Noto Sans JP", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(17, 66)
+        Me.Label2.Location = New System.Drawing.Point(17, 19)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(97, 25)
         Me.Label2.TabIndex = 35
@@ -108,7 +166,7 @@ Partial Class FormLeaderboard
         '
         Me.cmbSem.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmbSem.FormattingEnabled = True
-        Me.cmbSem.Location = New System.Drawing.Point(120, 67)
+        Me.cmbSem.Location = New System.Drawing.Point(120, 23)
         Me.cmbSem.Name = "cmbSem"
         Me.cmbSem.Size = New System.Drawing.Size(122, 24)
         Me.cmbSem.TabIndex = 34
@@ -118,7 +176,7 @@ Partial Class FormLeaderboard
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.Label1.Font = New System.Drawing.Font("Noto Sans JP", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(32, 20)
+        Me.Label1.Location = New System.Drawing.Point(32, 234)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(82, 25)
         Me.Label1.TabIndex = 33
@@ -129,7 +187,7 @@ Partial Class FormLeaderboard
         Me.btnFilter.BackColor = System.Drawing.SystemColors.ActiveBorder
         Me.btnFilter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnFilter.Font = New System.Drawing.Font("Noto Sans JP", 6.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnFilter.Location = New System.Drawing.Point(153, 355)
+        Me.btnFilter.Location = New System.Drawing.Point(37, 159)
         Me.btnFilter.Name = "btnFilter"
         Me.btnFilter.Size = New System.Drawing.Size(89, 34)
         Me.btnFilter.TabIndex = 32
@@ -140,30 +198,10 @@ Partial Class FormLeaderboard
         '
         Me.cmbSect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmbSect.FormattingEnabled = True
-        Me.cmbSect.Location = New System.Drawing.Point(120, 21)
+        Me.cmbSect.Location = New System.Drawing.Point(120, 235)
         Me.cmbSect.Name = "cmbSect"
         Me.cmbSect.Size = New System.Drawing.Size(122, 24)
         Me.cmbSect.TabIndex = 31
-        '
-        'Panel3
-        '
-        Me.Panel3.BackColor = System.Drawing.Color.DarkKhaki
-        Me.Panel3.Location = New System.Drawing.Point(0, 156)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(261, 10)
-        Me.Panel3.TabIndex = 25
-        '
-        'btnClear
-        '
-        Me.btnClear.BackColor = System.Drawing.Color.Red
-        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnClear.Font = New System.Drawing.Font("Noto Sans JP", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnClear.Location = New System.Drawing.Point(37, 355)
-        Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(89, 34)
-        Me.btnClear.TabIndex = 38
-        Me.btnClear.Text = "Clear"
-        Me.btnClear.UseVisualStyleBackColor = False
         '
         'FormLeaderboard
         '
@@ -195,4 +233,7 @@ Partial Class FormLeaderboard
     Friend WithEvents cmbSem As ComboBox
     Friend WithEvents Panel3 As Panel
     Friend WithEvents btnClear As Button
+    Friend WithEvents btnOV As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents cmbSub As ComboBox
 End Class
--- FormLeaderboard.vb
+++ FormLeaderboard.vb
@@ -45,16 +45,53 @@ Public Class FormLeaderboard
         Dim yearFilter As String = Form1.LoginSchoolYear.Trim()
         Dim semFilter As String = cmbSem.Text.Trim()
         Dim termFilter As String = cmbTerm.Text.Trim()
-        LoadLeaderboard(sectionFilter, yearFilter, semFilter, termFilter)
+
+        Select Case termFilter
+            Case "Prelim"
+                LoadPrelim(sectionFilter, yearFilter, semFilter)
+            Case "Midterm"
+                LoadMidterm(sectionFilter, yearFilter, semFilter)
+            Case "Final"
+                LoadFinal(sectionFilter, yearFilter, semFilter)
+        End Select
     End Sub
 
-    Private Sub LoadLeaderboard(section As String, yearLevel As String, semester As String, term As String)
+    Private Sub btnOV_Click(sender As Object, e As EventArgs) Handles btnOV.Click
+        Dim sectionFilter As String = cmbSect.Text.Trim()
+        Dim yearFilter As String = Form1.LoginSchoolYear.Trim()
+        Dim semFilter As String = cmbSem.Text.Trim()
+
+        If String.IsNullOrEmpty(sectionFilter) Then
+            MsgBox("Please select a section.", vbExclamation, "Validation Error")
+            Exit Sub
+        End If
+
+        If String.IsNullOrEmpty(semFilter) Then
+            MsgBox("Please select a semester.", vbExclamation, "Validation Error")
+            Exit Sub
+        End If
+        LoadOverall(sectionFilter, yearFilter, semFilter)
+    End Sub
+
+    Private Sub LoadPrelim(section As String, yearLevel As String, semester As String)
+        LoadRawTerm(section, yearLevel, semester, "Prelim")
+    End Sub
+
+    Private Sub LoadMidterm(section As String, yearLevel As String, semester As String)
+        LoadRawTerm(section, yearLevel, semester, "Midterm")
+    End Sub
+
+    Private Sub LoadFinal(section As String, yearLevel As String, semester As String)
+        LoadRawTerm(section, yearLevel, semester, "Final")
+    End Sub
+
+    Private Sub LoadRawTerm(section As String, yearLevel As String, semester As String, term As String)
         If cn.State = ConnectionState.Open Then cn.Close()
         cn.Open()
 
         sql = "SELECT u.UserID, u.Firstname, u.Lastname, u.Section, u.SchoolYear, " &
-              "ROUND((SUM(CASE WHEN s.Category IN ('Quiz','Activity','Assignment') THEN s.Score ELSE 0 END) * 2 + " &
-              "MAX(CASE WHEN s.Category = 'Exam' THEN s.Score ELSE 0 END)) / 3, 2) AS ComputedGrade " &
+              "ROUND((SUM(CASE WHEN s.Category IN ('Quiz','Activity','Assignment') THEN s.Score ELSE 0 END) * 0.4 + " &
+              "MAX(CASE WHEN s.Category = 'Exam' THEN s.Score ELSE 0 END) * 0.6), 2) AS ComputedGrade " &
               "FROM tblUser u JOIN tblScore s ON u.UserID = s.UserID "
 
         Dim filters As New List(Of String)
@@ -63,12 +100,9 @@ Public Class FormLeaderboard
         If semester <> "" Then filters.Add("s.Semester = @Semester")
         If term <> "" Then filters.Add("s.Term = @Term")
 
-        If filters.Count > 0 Then
-            sql &= "WHERE " & String.Join(" AND ", filters)
-        End If
+        If filters.Count > 0 Then sql &= " WHERE " & String.Join(" AND ", filters)
 
-        sql &= " GROUP BY u.UserID, u.Firstname, u.Lastname, u.Section, u.SchoolYear " &
-               " ORDER BY ComputedGrade DESC"
+        sql &= " GROUP BY u.UserID, u.Firstname, u.Lastname, u.Section, u.SchoolYear ORDER BY ComputedGrade DESC"
 
         cmd = New SqlCommand(sql, cn)
         If section <> "" Then cmd.Parameters.AddWithValue("@Section", section)
@@ -81,10 +115,53 @@ Public Class FormLeaderboard
         adpt.Fill(tbl)
         dvLB.DataSource = tbl
 
-        If dvLB.Columns.Contains("UserID") Then
-            dvLB.Columns("UserID").Visible = False
-        End If
-
+        If dvLB.Columns.Contains("UserID") Then dvLB.Columns("UserID").Visible = False
         cn.Close()
     End Sub
+
+    Private Sub LoadOverall(section As String, yearLevel As String, semester As String)
+        If cn.State = ConnectionState.Open Then cn.Close()
+        cn.Open()
+
+        sql = "SELECT u.UserID, u.Firstname, u.Lastname, u.Section, u.SchoolYear, " &
+              "ROUND((" &
+              "(SUM(CASE WHEN s.Category IN ('Quiz','Activity','Assignment') THEN s.Score ELSE 0 END) * 0.4 + " &
+              "MAX(CASE WHEN s.Category = 'Exam' THEN s.Score ELSE 0 END) * 0.6) + " &
+              "ISNULL((" &
+              "SELECT ROUND((" &
+              "(SUM(CASE WHEN s2.Category IN ('Quiz','Activity','Assignment') THEN s2.Score ELSE 0 END) * 0.4 + " &
+              "MAX(CASE WHEN s2.Category = 'Exam' THEN s2.Score ELSE 0 END) * 0.6) + " &
+              "ISNULL((" &
+              "SELECT ROUND((" &
+              "SUM(CASE WHEN s3.Category IN ('Quiz','Activity','Assignment') THEN s3.Score ELSE 0 END) * 0.4 + " &
+              "MAX(CASE WHEN s3.Category = 'Exam' THEN s3.Score ELSE 0 END) * 0.6), 2) " &
+              "FROM tblScore s3 WHERE s3.UserID = u.UserID AND s3.Semester = s.Semester AND s3.Term = 'Prelim' GROUP BY s3.UserID), 0) / 2" &
+              "), 2) " &
+              "FROM tblScore s2 WHERE s2.UserID = u.UserID AND s2.Semester = s.Semester AND s2.Term = 'Midterm' GROUP BY s2.UserID), 0) / 2" &
+              "), 2) AS ComputedGrade " &
+              "FROM tblUser u JOIN tblScore s ON u.UserID = s.UserID "
+
+        Dim filters As New List(Of String)
+        If section <> "" Then filters.Add("u.Section = @Section")
+        If yearLevel <> "" Then filters.Add("u.SchoolYear = @Year")
+        If semester <> "" Then filters.Add("s.Semester = @Semester")
+
+        If filters.Count > 0 Then sql &= " WHERE " & String.Join(" AND ", filters)
+
+        sql &= " GROUP BY u.UserID, u.Firstname, u.Lastname, u.Section, u.SchoolYear, s.Semester ORDER BY ComputedGrade DESC"
+
+        cmd = New SqlCommand(sql, cn)
+        If section <> "" Then cmd.Parameters.AddWithValue("@Section", section)
+        If yearLevel <> "" Then cmd.Parameters.AddWithValue("@Year", yearLevel)
+        If semester <> "" Then cmd.Parameters.AddWithValue("@Semester", semester)
+
+        Dim adpt As New SqlDataAdapter(cmd)
+        Dim tbl As New DataTable()
+        adpt.Fill(tbl)
+        dvLB.DataSource = tbl
+
+        If dvLB.Columns.Contains("UserID") Then dvLB.Columns("UserID").Visible = False
+        cn.Close()
+    End Sub
+
 End Class
